c17e262a8bc84fd0ea7a18bcfcf5a7dfbbad2996
allswellthatsmaxwell9@gmail.com <allswellthatsmaxwell9@gmail.com>
2018-02-10 00:37:45 -0600

    put more code into functions; modeled mean stars timeseries as well as review count series

--- R/model_yelp.R
+++ R/model_yelp.R
@@ -8,10 +8,12 @@ library(readr)
 library(lubridate)
 library(tis)
 
-filter_biz <- function(dat, businesses_to_keep) {
-  dplyr::inner_join(dat, businesses_to_keep, by = "business_id")
+#' Only keep the business IDs in businesses_to_use
+filter_biz <- function(dat, businesses_to_use) {
+  dplyr::inner_join(dat, businesses_to_use, by = "business_id")
 }
 
+#' Read csv_basename from elsewhere-defined DATA_DIR
 read_dat <- function(csv_basename) {
   readr::read_csv(glue("{DATA_DIR}/{csv_basename}"),
                   progress = FALSE)
@@ -26,10 +28,9 @@ copy_prophet_plot <- list(geom_ribbon(aes(ymin = yhat_lower,
                           geom_point(na.rm = TRUE),
                           geom_line(aes(y = yhat),
                                     color = "#0072B2",
-                                    na.rm = TRUE),
-                          theme(aspect.ratio = 3/5))
+                                    na.rm = TRUE))
 
-## Make the usual plot.prophet plot, but repeat in facets for every state.
+#' Make the usual plot.prophet plot, but repeat in facets for every state.
 plot_prophet_facets <- function(models, ylabel) {
   prophet_frames_by_state <-
     Map(function(state, model, fcast) {
@@ -40,7 +41,7 @@ plot_prophet_facets <- function(models, ylabel) {
   prophet_frames_by_state %>%
     ggplot(aes(x = as.Date(ds), y = y)) +
     facet_wrap(~state, scales = "free_y") +
-    ##theme_bw() +
+    theme_bw() +
     ylab(ylabel) +
     xlab("date") +
     scale_x_date(date_breaks = "6 months",
@@ -49,26 +50,64 @@ plot_prophet_facets <- function(models, ylabel) {
     copy_prophet_plot
 }
 
+#' Cast numeric date of the form yyyymmdd to a proper date.
+yyyymmdd_to_date <- . %>% as.character() %>% as.Date(format = "%Y%m%d")
 
-yyyymm_to_date <- . %>% as.character() %>% as.Date(format = "%Y%m%d")
-
+#' Get holidays.
 get_holidays <- function(years) {
   hol_vec <- holidays(businessOnly = FALSE, board = TRUE, years = years)
-  tibble(holiday = names(hol_vec), ds = yyyymm_to_date(hol_vec))
+  tibble(holiday = names(hol_vec), ds = yyyymmdd_to_date(hol_vec))
 }
+
+#' Construct daily-level fits, and forecast horizon days,
+#' for each state in state_day_frame.
+#' @param state_day_frame a dataframe with the columns
+#' state, ds (a date), and y (response variable; numeric)
+#' @param holiday_frame a dataframe with the columns "holiday" and "ds",
+#' respectively the name of the holiday and the dates it fell on.
+#' @param horizon scalar int; number of out-of-sample days in the future to
+#' be forecasted
+model_var_by_state <- function(state_day_frame,
+                               holiday_frame,
+                               horizon = 365 * 2) {
+  models <- state_day_frame %>%
+    select(state, ds, y) %>%
+    group_by(state) %>%
+    do(model = prophet(df = ., holidays = holiday_frame))
+  models$future <- lapply(models$model,
+                          function(m) make_future_dataframe(m, horizon))
+  models$forecast <- Map(predict, models$model, models$future)
+  models
+}
+
+#' Return only those states in dat that have at least min_reviews in year yyyy.
+apply_reviews_in_year_criteria <- function(dat, min_reviews, yyyy = 2017) {
+  dat %>%
+    group_by(state, year = lubridate::year(date)) %>%
+    summarize(reviews_in_year = n()) %>%
+    filter(year == yyyy, reviews_in_year >= min_reviews) %>%
+    select(state)
+}
+
+#' Common ggplot elements of simple raw timeseries plots.
+facet_pt_state_date <- list(geom_point(aes(x = date)),
+                            facet_wrap(~state, scales = "free_y"),
+                            theme_bw())
+
+#' Common states filter
+additional_states_filter <- . %>% filter(TRUE) ##filter(state %in% c("AZ", "PA"))
 
 
 DATA_DIR <- "../data"
 MIN_REVIEWS_IN_2017 <- 300
 
 businesses <- read_dat("yelp_business.csv")
-businesses_to_keep <- businesses %>%
-  select(business_id) %>%
-  unique()
+businesses_to_use <- businesses %>% select(business_id) %>% unique() ## top_n
 
-## checkins <- read_dat("yelp_checkin.csv") %>% filter_biz(businesses_to_keep)
-reviews <- read_dat("yelp_review.csv") %>% filter_biz(businesses_to_keep)
+## checkins <- read_dat("yelp_checkin.csv") %>% filter_biz(businesses_to_use)
 ## users <- read_dat("yelp_user.csv")
+
+reviews <- read_dat("yelp_review.csv") %>% filter_biz(businesses_to_use)
 
 reviews %<>% select(-text)
 
@@ -77,54 +116,49 @@ businesses_and_reviews <- businesses %>%
   select(business_id, name, review_count, state, city) %>%
   inner_join(reviews, by = "business_id")
 
-rm(reviews)
-rm(businesses)
+rm(reviews);rm(businesses);gc()
 
-states_with_many_reviews <- businesses_and_reviews %>%
-  group_by(state, year = year(date)) %>%
-  summarize(reviews_in_year = n()) %>%
-  filter(year == 2017, reviews_in_year >= MIN_REVIEWS_IN_2017)
+states_to_use <- businesses_and_reviews %>%
+  apply_reviews_in_year_criteria(min_reviews = MIN_REVIEWS_IN_2017, yyyy = 2017)
 
 state_review_values_by_date <- businesses_and_reviews %>%
-  inner_join(states_with_many_reviews, by = "state") %>%
+  inner_join(states_to_use, by = "state") %>%
   group_by(state, date) %>%
   summarize(reviews = n(), mean_stars = mean(stars)) %>%
-  group_by(state) %>%
   arrange(state, date)
 
+## Vanilla plots of daily values.
+daily_avg_stars_plot <- state_review_values_by_date %>%
+  ggplot(aes(y = mean_stars)) +
+  facet_pt_state_date
 daily_review_counts_plot <- state_review_values_by_date %>%
-  ggplot(aes(x = date, y = reviews)) +
-  geom_line() +
-  facet_wrap(~state, scales = "free_y") +
-  theme_bw()
+  ggplot(aes(y = reviews)) +
+  facet_pt_state_date
 
+## Modeling.
+##
+## Holiday drops are pretty drastic in a few states; adding in holidays
+## induces prophet to drop its forecast more aggresively on holidays
+## corresponding to those historical dips.
 hols <- get_holidays(unique(year(state_review_values_by_date$date)))
 
-models <- state_review_values_by_date %>%
+reviews_models <- state_review_values_by_date %>%
+  additional_states_filter() %>%
   rename(ds = date, y = reviews) %>%
-  select(-mean_stars) %>%
-  group_by(state) %>%
-  do(model = prophet(df = ., holidays = hols))
+  model_var_by_state(hols)
 
-models$future <- lapply(models$model, function(m) make_future_dataframe(m, 1000))
-models$forecast <- Map(predict, models$model, models$future)
+## Great!
+reviews_facets <- plot_prophet_facets(reviews_models,
+                                      ylab = "reviews posted on day")
 
-## The model is a little more hesitant about holiday dips than I'd like.
-## I feel like the dips in AZ in particular can be more aggressive. Let's
-## try adding in holidays to the model (can use just AZ for testing)
-## to see if that ups the aggression.
-prophet_facets <- plot_prophet_facets(models, ylab = "reviews posted on day")
+stars_models <- state_review_values_by_date %>%
+  additional_states_filter() %>%
+  rename(ds = date, y = mean_stars) %>%
+  model_var_by_state(hols)
 
-
-daily_avg_stars_plot <- state_review_values_by_date %>%
-  ggplot(aes(x = date, y = mean_stars)) +
-  geom_line() +
-  facet_wrap(~state) +
-  theme_bw()
-
-
-
-
-
-
-
+## So there might be some seasonal components to rating values.
+## How interesting are the sharp downward spikes around Christmas (?) in PA?
+## Seems a little overzealous.
+## Next, let's pull out each component and look at them.
+stars_facets <- plot_prophet_facets(stars_models,
+                                    ylab = "Mean stars of ratings posted on day")
