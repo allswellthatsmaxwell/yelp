383111309583b40788218a3a8f58c863b75f5824
allswellthatsmaxwell9@gmail.com <allswellthatsmaxwell9@gmail.com>
2018-02-27 23:59:05 -0600

    got the rest of the coursera helper base functions in there

--- reviews_content/py/nn_test.py
+++ reviews_content/py/nn_test.py
@@ -6,56 +6,6 @@ Created on Tue Feb 27 21:31:31 2018
 @author: mson
 """
 
-def linear_forward_test_case():
-    np.random.seed(1)
-    """
-    X = np.array([[-1.02387576, 1.12397796],
- [-1.62328545, 0.64667545],
- [-1.74314104, -0.59664964]])
-    W = np.array([[ 0.74505627, 1.97611078, -1.24412333]])
-    b = np.array([[1]])
-    """
-    A = np.random.randn(3,2)
-    W = np.random.randn(1,3)
-    b = np.random.randn(1,1)
-    
-    return A, W, b
-
-def linear_activation_forward_test_case():
-    """
-    X = np.array([[-1.02387576, 1.12397796],
- [-1.62328545, 0.64667545],
- [-1.74314104, -0.59664964]])
-    W = np.array([[ 0.74505627, 1.97611078, -1.24412333]])
-    b = 5
-    """
-    np.random.seed(2)
-    A_prev = np.random.randn(3,2)
-    W = np.random.randn(1,3)
-    b = np.random.randn(1,1)
-    return A_prev, W, b
-
-def L_model_forward_test_case_2hidden():
-    np.random.seed(6)
-    X = np.random.randn(5,4)
-    W1 = np.random.randn(4,5)
-    b1 = np.random.randn(4,1)
-    W2 = np.random.randn(3,4)
-    b2 = np.random.randn(3,1)
-    W3 = np.random.randn(1,3)
-    b3 = np.random.randn(1,1)
-  
-    parameters = {1: {"W": W1, "b": b1},
-                  2: {"W": W2, "b": b2},
-                  3: {"W": W3, "b": b3}}
-    
-    return X, parameters
-
-def compute_cost_test_case():
-    Y = np.asarray([[1, 1, 1]])
-    aL = np.array([[.8,.9,0.4]])
-    
-    return Y, aL
 
 np.random.seed(1)
 parameters = initialize_parameters([5,4,3])
@@ -71,10 +21,45 @@ print("With sigmoid: A = " + str(A))
 A, linear_activation_cache = linear_activation_forward(A_prev, W, b, activation = relu)
 print("With ReLU: A = " + str(A))
 
+activations = [relu, relu, relu, sigmoid]
+
 X, parameters = L_model_forward_test_case_2hidden()
-AL, caches = L_model_forward(X, parameters, [relu, relu, relu, sigmoid])
+AL, caches = L_model_forward(X, parameters, activations)
 print("AL = " + str(AL))
 print("Length of caches list = " + str(len(caches)))
 
 Y, AL = compute_cost_test_case()
 print("cost = " + str(compute_cost(AL, Y)))
+
+dZ, linear_cache = linear_backward_test_case()
+dA_prev, dW, db = linear_backward(dZ, linear_cache)
+print ("dA_prev = "+ str(dA_prev))
+print ("dW = " + str(dW))
+print ("db = " + str(db))
+
+
+dAL, linear_activation_cache = linear_activation_backward_test_case()
+
+dA_prev, dW, db = linear_activation_backward(dAL, linear_activation_cache, activation = sigmoid)
+print ("sigmoid:")
+print ("dA_prev = "+ str(dA_prev))
+print ("dW = " + str(dW))
+print ("db = " + str(db) + "\n")
+
+dA_prev, dW, db = linear_activation_backward(dAL, linear_activation_cache, activation = relu)
+print ("relu:")
+print ("dA_prev = "+ str(dA_prev))
+print ("dW = " + str(dW))
+print ("db = " + str(db))
+
+AL, Y_assess, caches = L_model_backward_test_case()
+grads = L_model_backward(AL, Y_assess, caches, activations)
+print_grads(grads)
+
+parameters, grads = update_parameters_test_case()
+parameters = update_parameters(parameters, grads, 0.1)
+
+print ("W1 = "+ str(parameters["W1"]))
+print ("b1 = "+ str(parameters["b1"]))
+print ("W2 = "+ str(parameters["W2"]))
+print ("b2 = "+ str(parameters["b2"]))
